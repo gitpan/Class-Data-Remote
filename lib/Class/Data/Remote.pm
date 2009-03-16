3d71b146efbcdec6786f0e67bdc21f5fc25ff28b
John Drago <jdrago_999@yahoo.com>
2009-03-16 02:21:54 +0000

    Import of JOHND/Class-Data-Remote-0.001 from CPAN.
    gitpan-cpan-distribution: Class-Data-Remote
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         JOHND/Class-Data-Remote-0.001.tar.gz
    gitpan-cpan-author:       JOHND
    gitpan-cpan-maturity:     released

---
+++ lib/Class/Data/Remote.pm
@@ -0,0 +1,36 @@
+
+package Class::Data::Remote;
+
+use strict;
+use warnings 'all';
+use vars qw( $VERSION );
+
+$VERSION = '0.001';
+
+1;# return true:
+
+=pod
+
+=head1 NAME
+
+Class::Data::Remote - Object-oriented interface to remote data sources.
+
+=head1 DESCRIPTION
+
+Class::Data::Remote will provide an interface to access remote data sources.
+
+=head1 AUTHOR
+
+John Drago <jdrago_999@yahoo.com>
+
+=head1 COPYRIGHT
+
+Copyright 2009 John Drago <jdrago_999@yahoo.com> all rights reserved.
+
+=head1 LICENSE
+
+This software is free software and may be used and redistributed under the same
+terms as Perl itself.
+
+=cut
+
